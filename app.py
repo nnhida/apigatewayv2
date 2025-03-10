02095899ea210204c9b544a0c26b728a44a45c96
nnhida <nurhidayahayundira296@gmail.com>
2025-03-10 13:33:23 +0700

    push

--- app.py
+++ app.py
@@ -102,4 +102,4 @@ def update_user(user_id):
         return jsonify({"error": "Failed to update user"}), response.status_code
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port='5000')
